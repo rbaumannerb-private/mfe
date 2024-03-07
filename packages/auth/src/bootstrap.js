05d28c31b15c9d5c5dc4264c1c03416754e3ab2b
Ron Baumann-Erb <rbaumann@mobilehealth.net>
2024-03-07 14:31:25 -0500

    Added Auth sub app

--- packages/auth/src/bootstrap.js
+++ packages/auth/src/bootstrap.js
@@ -4,32 +4,37 @@ import { createMemoryHistory, createBrowserHistory } from 'history';
 import App from './App';
 
 // Mount function to start up the app
-const mount = (el, { onNavigate, defaultHistory, initialPath }) => {
-  const history = defaultHistory || createMemoryHistory( { initialEntries: [initialPath] });
+const mount = (el, { onSignIn, onNavigate, defaultHistory, initialPath }) => {
+  const history =
+    defaultHistory ||
+    createMemoryHistory({
+      initialEntries: [initialPath],
+    });
 
   if (onNavigate) {
     history.listen(onNavigate);
   }
 
-  ReactDOM.render(<App history={history} />, el);
+  ReactDOM.render(<App onSignIn={onSignIn} history={history} />, el);
 
   return {
     onParentNavigate({ pathname: nextPathname }) {
       const { pathname } = history.location;
-      if (pathname !== nextPathname){
+
+      if (pathname !== nextPathname) {
         history.push(nextPathname);
       }
-    }
-  }
+    },
+  };
 };
 
 // If we are in development and in isolation,
 // call mount immediately
 if (process.env.NODE_ENV === 'development') {
-  const devRoot = document.querySelector('#_marketing-dev-root');
+  const devRoot = document.querySelector('#_auth-dev-root');
 
   if (devRoot) {
-    mount(devRoot, { defaultHistory: createBrowserHistory() }); // use browser history instead of memory history
+    mount(devRoot, { defaultHistory: createBrowserHistory() });
   }
 }
 
